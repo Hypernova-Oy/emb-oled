ecb2fc64f79e5da8ef8af8710492c7264ddd1758
Olli-Antti Kivilahti <olli-antti.kivilahti@jns.fi>
2016-10-04 16:08:13 +0300

    Forgot the config file installation

--- make.sh
+++ make.sh
@@ -3,3 +3,12 @@ perl ./Build.PL
 perl ./Build installdeps
 perl ./Build install
 perl ./Build realclean
+
+confDir="etc/emb-oled"
+mkdir -p /$confDir
+
+if [ ! -e /$confDir/server.conf ]; #Copy file only if not present
+then
+    cp $confDir/server.conf /$confDir/server.conf
+fi
+
