40a692c8e23d2a78ae49d81af02e71c17571c42d
Olli-Antti Kivilahti <olli-antti.kivilahti@jns.fi>
2016-10-04 16:32:38 +0300

    Added systemd service

--- make.sh
+++ make.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+
 perl ./Build.PL
 perl ./Build installdeps
 perl ./Build install
@@ -6,11 +7,20 @@ perl ./Build realclean
 
 confDir="etc/emb-oled"
 binDir="/usr/local/bin"
-mkdir -p /$confDir
+systemdServiceDir="etc/systemd/system"
 
+
+mkdir -p /$confDir
 if [ ! -e /$confDir/server.conf ]; #Copy file only if not present
 then
     cp $confDir/server.conf /$confDir/server.conf
 fi
 
-cp scripts/oled_server $binDir/
+
+cp scripts/oled_server $binDir/oled_server
+
+
+cp $systemdServiceDir/emb-oled.service /$systemdServiceDir/emb-oled.service
+systemctl daemon-reload
+systemctl enable emb-oled
+systemctl start emb-oled
